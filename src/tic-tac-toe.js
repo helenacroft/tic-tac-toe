9636d643bd6ce95a8ca1079a3ba80a4d96e467e2
Helen <helenyarinovskaya@gmail.com>
2017-01-31 02:19:01 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -27,7 +27,7 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+        return this.emptyFields ? false : true;
     }
 
     isDraw() {
@@ -35,6 +35,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
+        return this.matrix[rowIndex][colIndex] ? this.matrix[rowIndex][colIndex] : null;
 
     }
 }
