a33727d824d869cd821f087512722e02331e7719
Helen <helenyarinovskaya@gmail.com>
2017-02-01 00:15:42 +0300

    tic-tac-toe/feature:
    getWinner, checkDiagonals and checkRowsAndColumns methods added

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -18,9 +18,32 @@ class TicTacToe {
     isFinished() {
 
     }
+    checkRowsAndColumns() {
+        for(var i = 0; i < 3; i++) {            
+            if(this.matrix[i][0] == this.matrix[i][1] && this.matrix[i][1] == this.matrix[i][2]) {
+                return this.matrix[i][0];                
+            }  
+            if(this.matrix[0][i] == this.matrix[1][i] && this.matrix[1][i] == this.matrix[2][i]) {                 
+                return this.matrix[0][i];
+            }          
+        }
+        return null;
+    }
+    
+    checkDiagonals() {        
+        if(this.matrix[0][0] == this.matrix[1][1] && this.matrix[1][1]== this.matrix[2][2]) {
+             return this.matrix[0][0];
+         }
+             
+        if(this.matrix[0][2] == this.matrix[1][1] && this.matrix[1][1] == this.matrix[2][0]) {
+             return this.matrix[0][2];             
+        }  
+        return null;
+    }
 
     getWinner() {
-
+        var winner = this.checkRowsAndColumns() || this.checkDiagonals(); 
+        return winner;  
     }
 
     noMoreTurns() {
